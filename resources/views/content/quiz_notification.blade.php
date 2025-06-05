227f3c7a9b6b783b9a0823705484bfb3f1882ec3
Sunny Baghel <Sunny@svrind.local>
2025-06-05 14:44:51 +0530

    reattempt message

--- resources/views/content/quiz_notification.blade.php
+++ resources/views/content/quiz_notification.blade.php
@@ -2,6 +2,6 @@
     <div class="container">
     <p>We noticed that your quiz session was unexpectedly interrupted and could not be completed successfully. This may have occurred due to a technical issue or an unstable internet connection. Unfortunately, your attempt could not be recorded as complete.</p>
 
-    <p>If you would like to attempt the quiz again, please get in touch with our support team by emailing us at nep@asdc.com. Our team will assist you in rescheduling or reactivating your quiz attempt. We appreciate your understanding and apologize for any inconvenience caused.</p>
+    <p>If you would like to attempt the quiz again, please get in touch with our support team by emailing us at <b><a href="mailto:nep@asdc.org.in">nep@asdc.org.in</a></b> Our team will assist you in rescheduling or reactivating your quiz attempt. We appreciate your understanding and apologize for any inconvenience caused.</p>
     </div>
 </section>
